391f43f253ce63ec8c051736af39481fa79d668e
Anthony <Anthony.Fitzsimons@ericsson.com>
2013-09-30 15:40:38 +0100

    greetings.py

--- greetings.py
+++ greetings.py
@@ -1,9 +1,11 @@
 'There is a hi function and a bye functon'
 
+
 def hi():
     'Hi, no parameters in this method'
     print 'Hi'
 
 def bye():
+    
     'No parameters here either'
     print 'Bye'
